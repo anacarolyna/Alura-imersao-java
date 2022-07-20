5335ffbcf0757600daba60374c89aa01e793f9b1
anacarolyna <anacarolynadinizr@gmail.com>
2022-07-19 21:03:00 -0300

    Aula 2

--- src/App.java
+++ src/App.java
@@ -1,4 +1,6 @@
+import java.io.InputStream;
 import java.net.URI;
+import java.net.URL;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
@@ -26,16 +28,21 @@ public class App {
         // System.out.println(listaDeFilmes.get( 0));
 
         //exibir e manipular os dados
+        var geradora = new GeradoraDeFigurinhas();
         for (Map<String,String> filme : listaDeFilmes) {
-            System.out.println(filme.get( "title"));
-            System.out.println(filme.get( "image"));
-            System.out.println(filme.get( "imDbrating"));
+
+            String urlImagem = filme.get("image");
+            String titulo = filme.get("title");
+
+            InputStream inputStream = new URL(urlImagem).openStream();
+            String nomeArquivo = titulo + ".png";
+
+            geradora.cria(inputStream, nomeArquivo);
+
+            System.out.println(titulo);
             System.out.println();
             
         }
-
-        
-
 
     }
 }
--- src/GeradoraDeFigurinhas.java
+++ src/GeradoraDeFigurinhas.java
@@ -1,26 +1,49 @@
+import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.awt.Font;
+import java.awt.Color;
 
 import javax.imageio.ImageIO;
 
 public class GeradoraDeFigurinhas {
 
-    public void cria() throws Exception{
+    
+    public void cria(InputStream inputStream, String nomeArquivo) throws Exception{
       
     //leitura da imagem
-    BufferedImage imagemOriginal = ImageIO.read(new File("entrada/filme.jpg"));
+    // InputStream inputStream = new FileInputStream(new File("entrada/filme.jpg"));
+    // InputStream inputStream = new URL("https://imersao-java-apis.s3.amazonaws.com/TopMovies_1.jpg").openStream();
+    BufferedImage imagemOriginal = ImageIO.read(inputStream);
 
 
     //cria nova imagem em memória com transparência e com tamanho novo
-     imagemOriginal
+     int largura = imagemOriginal.getWidth();
+     int altura = imagemOriginal.getHeight();
+     int novaAltura = altura + 200;
+     BufferedImage novaImagem = new BufferedImage(largura, novaAltura, BufferedImage.TRANSLUCENT);
     
     //copiar a imagem original para nova imagem (em memória)
+    Graphics2D graphics =(Graphics2D) novaImagem.getGraphics();
+    graphics.drawImage(imagemOriginal, 0, 0, null);
+
+     // configurar a fonte
+     var fonte = new Font(Font.SANS_SERIF, Font.BOLD, 64);
+     graphics.setColor(Color.YELLOW);
+     graphics.setFont(fonte);
+
 
     //escrever uma frase na nova imagem
+    graphics.drawString("TOPZERA", 0, novaAltura - 100);
 
     //escrever a nova imagem em um arquivo
+    ImageIO.write(novaImagem, "png", new File(nomeArquivo));
 
     }
+
     
 }
